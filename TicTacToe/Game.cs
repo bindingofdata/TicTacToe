0212033656614d3865a12d403ed72d1cbd2f34c9
Hunter Curren <hcurren82@hotmail.com>
2018-10-11 17:26:59 -0700

    Added input validation. Added varification that the selected location is available.
    The program should now meet the requirements of the project.

--- TicTacToe/Game.cs
+++ TicTacToe/Game.cs
@@ -61,37 +61,112 @@ namespace TicTacToe
 
         private void UpdateBoard()
         {
-            if (CurrentTurnPlayer == 1)
-                Console.WriteLine($"{Player1.Name}'s Turn");
-            else
-                Console.WriteLine($"{Player2.Name}'s Turn");
+            bool validInput = false;
+            bool locationFree = false;
+            char[] userSelection;
+            int[] tileCoordinates;
 
-            Console.Write("Enter your move Letter then Number [eg A1]: ");
-            char[] userSelection = Console.ReadLine().ToCharArray();
-            int[] tileCoordinates = new int[2];
+            while (!validInput)
+            {
+                if (CurrentTurnPlayer == 1)
+                    Console.WriteLine($"{Player1.Name}'s Turn");
+                else
+                    Console.WriteLine($"{Player2.Name}'s Turn");
+
+                Console.Write("Enter your move (Letter then Number [eg A1]): ");
+                userSelection = Console.ReadLine().ToCharArray();
+                if (userSelection.Length > 0)
+                    validInput = ValidateInpute(userSelection);
+
+                if (validInput)
+                {
+                    tileCoordinates = new int[2];
+
+                    switch (userSelection[0])
+                    {
+                        case 'A':
+                        case 'a':
+                            userSelection[0] = '0';
+                            break;
+                        case 'B':
+                        case 'b':
+                            userSelection[0] = '1';
+                            break;
+                        case 'C':
+                        case 'c':
+                            userSelection[0] = '2';
+                            break;
+                        default:
+                            break;
+                    }
+
+                    int.TryParse(userSelection[0].ToString(), out tileCoordinates[0]);
+                    int.TryParse(userSelection[1].ToString(), out tileCoordinates[1]);
+
+                    locationFree = CheckLocation(tileCoordinates[0], tileCoordinates[1] - 1);
+
+                    if (locationFree)
+                        board.SetTile(tileCoordinates[0], tileCoordinates[1] - 1, (TileState)CurrentTurnPlayer);
+                    else
+                    {
+                        validInput = false;
+                        Console.Clear();
+                        DrawGame();
+                        Console.WriteLine("That location is already taken. Please select a different square.");
+                    } 
+                }
+                else
+                {
+                    Console.Clear();
+                    DrawGame();
+                    Console.WriteLine("Invalid input. Please enter a Letter followed by a Number [eg A1]");
+                }
+            } 
+        }
+
+        private bool CheckLocation(int x, int y)
+        {
+            return board.CurrentBoard[y, x] == " ";
+        }
+
+        private bool ValidateInpute(char[] userSelection)
+        {
+            bool inputIsValid = false;
+            bool validLetter = false;
+            bool validNumber = false;
 
             switch (userSelection[0])
             {
                 case 'A':
                 case 'a':
-                    userSelection[0] = '0';
-                    break;
                 case 'B':
                 case 'b':
-                    userSelection[0] = '1';
-                    break;
                 case 'C':
                 case 'c':
-                    userSelection[0] = '2';
+                    validLetter = true;
                     break;
                 default:
                     break;
             }
 
-            int.TryParse(userSelection[0].ToString(), out tileCoordinates[0]);
-            int.TryParse(userSelection[1].ToString(), out tileCoordinates[1]);
+            if (userSelection.Length > 1)
+            {
+                switch (userSelection[1])
+                {
+                    case '1':
+                    case '2':
+                    case '3':
+                        validNumber = true;
+                        break;
+                    default:
+                        break;
+                }
+            }
 
-            board.SetTile(tileCoordinates[0], tileCoordinates[1] - 1, (TileState)CurrentTurnPlayer);
+            if (validLetter == true && validNumber == true)
+                inputIsValid = true;
+
+            return inputIsValid;
         }
 
         private void ChangeTurns()
--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -10,53 +10,81 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
-            /*
-            Board board = new Board();
-            Player player1 = new Player(TileState.X, "Bob");
-            Player player2 = new Player(TileState.O, "Bobette");
-            //Renderer.DrawScreen(player1, player2, board);
-            Console.WriteLine(board.ToString());
-
-            Console.ReadKey();
-
-            board.SetTile(0, 0, TileState.X);
-            board.SetTile(1, 1, TileState.X);
-            board.SetTile(2, 0, TileState.O);
-            board.SetTile(2, 1, TileState.O);
-            //Renderer.DrawScreen(player1, player2, board);
-            Console.WriteLine(board.ToString());
-            */
+            // TODO: Add a game menu to allow playing more than one game in a row
 
             string player1 = "";
             string player2 = "";
             int winner = 0;
             Game game;
 
-            Console.WriteLine("TicTacToe!\n");
 
-            Console.Write("Player 1: ");
-            player1 = Console.ReadLine();
+            while (MainMenu())
+            {
+                Console.WriteLine("New Game: ");
+                Console.WriteLine("Please provide player names: ");
+                Console.Write("Player 1: ");
+                player1 = Console.ReadLine();
 
-            Console.Write("Player 2: ");
-            player2 = Console.ReadLine();
+                Console.Write("Player 2: ");
+                player2 = Console.ReadLine();
+
+                Console.Clear();
+
+                game = new Game(player1, player2);
+
+                winner = game.NewGame();
+
+                Console.WriteLine();
+                if (winner == 1)
+                    Console.WriteLine($"Congratulations {player1}! You won!");
+                else if (winner == 2)
+                    Console.WriteLine($"Congratulations {player1}! You won!");
+                else
+                    Console.WriteLine("Tie Game!");
+
+                Console.ReadKey();
+            }
 
             Console.Clear();
-
-            game = new Game(player1, player2);
-
-            winner = game.NewGame();
-
-            Console.WriteLine();
-            if (winner == 1)
-                Console.WriteLine($"Congratulations {player1}! You won!");
-            else if (winner == 2)
-                Console.WriteLine($"Congratulations {player1}! You won!");
-            else
-                Console.WriteLine("Tie Game!");
-
-
-
-            Console.ReadKey();
+            Console.WriteLine("Thanks for playing!");
         }
+
+        static bool MainMenu()
+        {
+            bool playGame = false;
+            bool validSelection = false;
+
+            while (!validSelection)
+            {
+                Console.WriteLine("TicTacToe!\n");
+                Console.WriteLine("Main Menu");
+                Console.WriteLine("1. New Game [N/n]");
+                Console.WriteLine("2. Exit [E/e]");
+                Console.Write("Enter selection -> ");
+                string selection = Console.ReadLine();
+
+                switch (selection)
+                {
+                    case "N":
+                    case "n":
+                    case "1":
+                        playGame = true;
+                        validSelection = true;
+                        break;
+                    case "E":
+                    case "e":
+                    case "2":
+                        validSelection = true;
+                        break;
+                    default:
+                        break;
+                }
+
+                Console.Clear();
+            }
+
+            return playGame;
+        }// MainMenu
+        
     }
 }
